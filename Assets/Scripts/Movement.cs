f7fc99b3b9d04f43de130cac6d735f7cfeebf51d
Victor Kudelin <58315363+K-vic98@users.noreply.github.com>
2020-11-30 08:33:37 +0300

    Did the mechanics of movement
    Implemented the mechanics of moving and turning the player

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private CharacterController _characterController;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _speedRotation;
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * _speedRotation, 0); 
+        float curSpeed = _speed * Input.GetAxis("Vertical");
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        _characterController.Move(curSpeed * forward);
+    }
+}
